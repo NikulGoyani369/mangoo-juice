04a31a7dc4bb40b0c16fae034f04367683147395
Nikul Goyani <goyaniniks@gmail.com>
2026-02-26 21:00:53 +0100

    fix: exclude e2e/ from Jest so Playwright specs don't break unit test run

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,7 @@
 module.exports = {
     testEnvironment: 'jsdom',
+    // Only run Jest unit tests — ignore Playwright E2E specs
+    testMatch: ['**/*.test.js'],
+    testPathIgnorePatterns: ['/node_modules/', '/e2e/'],
 };
+
